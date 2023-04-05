4e9ae4510434e81fb91190e26011e1b118611437
Ashley-King <Ashley-King@users.noreply.github.com>
2023-04-05 09:34:16 -0400

    added posts to home page

--- blog-frontend/src/pages/index.js
+++ blog-frontend/src/pages/index.js
@@ -1,14 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../../client'
 
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+
+export default function Home({posts}) {
+  console.log("posts", posts[0])
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>Welcome to the blog!</h1>
+
+      {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href={`/posts/${encodeURIComponent(slug.current)}`}>
+                  {title}
+                </Link>{' '}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
     </div>
   )
 }
+
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+    *[_type == "post" ] | order(publishedAt desc)
+  `)
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
